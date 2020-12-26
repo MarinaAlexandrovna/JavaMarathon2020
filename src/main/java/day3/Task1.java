c56ae97d2eb8f7951df401a0f242f46926485f8d
Marina Sergievskaya <marinesergievskaya@gmail.com>
2020-12-26 21:18:45 +0300

    Complete day3 tasks

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -2,6 +2,41 @@ package day3;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите название города или stop, чтобы прервать программу");
+        String city = sc.nextLine();
+
+
+        switch (city) {
+            case "Москва":
+            case "Влодивосток":
+            case "Ростов":
+                System.out.println("Россия");
+                break;
+            case "Рим":
+            case "Милан":
+            case "Турин":
+                System.out.println("Италия");
+                break;
+            case "Ливерпуль":
+            case "Менчестер":
+            case "Лондон":
+                System.out.println("Англия");
+                break;
+            case "Берлин":
+            case "Мюнхен":
+            case "Кельн":
+                System.out.println("Гремания");
+                break;
+            default:
+                System.out.println("Неизвестная страна");
+            case "stop":
+                break;
+        }
+        System.out.println();
+    }
+}
+
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -2,6 +2,23 @@ package day3;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x, y;
+        int a = 1;
 
+        while (a <= 5) {
+            System.out.println("Пошел цикл " + a);
+            System.out.println("Введите число x");
+            x = sc.nextDouble();
+            System.out.println("Введите число y");
+            y = sc.nextDouble();
+
+            if (y == 0) {
+                System.out.println("Нельзя делить на ноль");
+                continue;
+            }
+            System.out.println("Результат = " + x / y);
+            a = a + 1;
+        }
     }
 }
