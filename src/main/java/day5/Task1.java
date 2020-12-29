dfa577ade5b90251326044ea01a1f02595ce01b3
Marina Sergievskaya <marinesergievskaya@gmail.com>
2020-12-29 08:25:53 +0300

    Complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,49 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setYearOfIssue(1985);
+        car1.setModel("W");
+        car1.setColor("Green");
+        car1.getYearOfIssue();
+        car1.getModel();
+        car1.getColor();
 
+        System.out.println("Выведем нашу машину на экран");
+        System.out.println();
+
+        System.out.println("Модель машины " + car1.getModel() + "." + "\n"
+                + "Год выпуска " + car1.getYearOfIssue() + "." + "\n"
+                + "Цвет " + car1.getColor() + ".");
+    }
+}
+
+class Car {
+    private int yearOfIssue;
+    private String color;
+    private String model;
+
+    public void setYearOfIssue(int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,33 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike(1998, "Grey", "Java");
+        motorbike1.getModel();
+        motorbike1.getYeatOfIssue();
+        motorbike1.getColor();
+    }
+}
 
+class Motorbike {
+    private int yearOfIssue;
+    private String color;
+    private String model;
+
+    public Motorbike(int yearOfIssue, String color, String model) {
+        this.yearOfIssue = yearOfIssue;
+        this.color = color;
+        this.model = model;
+    }
+
+    public void getModel() {
+        System.out.println("Модель мотоцикла - " + model);
+    }
+
+    public void getYeatOfIssue() {
+        System.out.println("Год выпуска - " + yearOfIssue);
+    }
+
+    public void getColor() {
+        System.out.println("Цвет - " + color);
     }
 }
